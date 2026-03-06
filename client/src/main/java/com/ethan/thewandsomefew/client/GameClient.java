9ac0961c51170cc6e9be3237db81678cd3f04115
Ethan Meli <etme6835@colorado.edu>
2026-03-06 01:57:06 -0700

    Create basic socket between server and client

--- client/src/main/java/com/ethan/thewandsomefew/client/GameClient.java
+++ client/src/main/java/com/ethan/thewandsomefew/client/GameClient.java
@@ -1,7 +1,33 @@
 package com.ethan.thewandsomefew.client;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+import com.ethan.thewandsomefew.protocol.HelloPacket;
+import com.ethan.thewandsomefew.protocol.PacketCodec;
+
+/**
+ * Game Client connects to Server Socket
+ * Define host IP and port to send Socket DataOutputStream
+ * Create PacketCodec object for efficient write
+ * Create Socket on defined host and port
+ * Create DataOutputStream from socket initially as BufferedOutputStream for optimization
+ * Send DataOutputStream to Socket
+ */
 public final class GameClient {
-  public static void main(String[] args) {
-    System.out.println("Client booted.");
+  public static void main(String[] args) throws Exception {
+    String host = "127.0.0.1";
+    int port = 43594;
+
+    PacketCodec codec = new PacketCodec();
+
+    try (Socket socket = new Socket(host, port)) {
+      System.out.println("Connected to server.");
+
+      DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+      codec.writePacket(out, new HelloPacket(258));
+      out.flush();
+    }
   }
 }
--- server/src/main/java/com/ethan/thewandsomefew/server/GameServer.java
+++ server/src/main/java/com/ethan/thewandsomefew/server/GameServer.java
@@ -1,7 +1,41 @@
 package com.ethan.thewandsomefew.server;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.ethan.thewandsomefew.protocol.HelloPacket;
+import com.ethan.thewandsomefew.protocol.Packet;
+import com.ethan.thewandsomefew.protocol.PacketCodec;
+
+/**
+ * Game Server creates Server Socket and listens on specified port
+ * PacketCodec object initialized to read input stream
+ * Create Socket on Server Socket accept
+ * Fetch DataInputStream initially as BufferedInputStream for optimization
+ * Read DataInput using codec object to return a Packet
+ */
 public final class GameServer {
-  public static void main(String[] args) {
-    System.out.println("Server booted.");
+  public static void main(String[] args) throws Exception {
+    int port = 43594;
+    PacketCodec codec = new PacketCodec();
+
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      System.out.println("Server listening on " + port);
+
+      try (Socket socket = serverSocket.accept()) {
+        System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+
+        DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        Packet inPacket = codec.readPacket(in);
+        
+        if (inPacket instanceof HelloPacket helloPacket) {
+          System.out.println("Protcol Version: " + helloPacket.protocolVersion());
+        } else {
+          System.out.println("Packet Type: " + inPacket.id().toString());
+        }
+      }
+    }
   }
 }
